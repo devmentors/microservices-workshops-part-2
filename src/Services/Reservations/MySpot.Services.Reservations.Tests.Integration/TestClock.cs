23ea68eca6b3befac60a78e317dd50e1baedcdb4
Dariusz Pawlukiewicz <dpawlukiewicz@ecovadis.com>
2022-06-20 15:38:21 +0200

    Added test clock

---
+++ src/Services/Reservations/MySpot.Services.Reservations.Tests.Integration/TestClock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Micro.Time;
+
+namespace MySpot.Services.Reservations.Tests.Integration;
+
+[ExcludeFromCodeCoverage]
+public class TestClock : IClock
+{
+    public DateTime Current()
+        => new(2022, 06, 20, 12, 0, 0);
+}
