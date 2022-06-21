e334e051758b3294264920132529d183a81cdd96
Dariusz Pawlukiewicz <dpawlukiewicz@ecovadis.com>
2022-06-21 13:08:46 +0200

    Added tests database for availability + provider tests

--- src/Services/Availability/MySpot.Services.Availability.Tests.Contract/Provider/ReservationsConsumerContractsTests.cs
+++ src/Services/Availability/MySpot.Services.Availability.Tests.Contract/Provider/ReservationsConsumerContractsTests.cs
@@ -2,19 +2,25 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Micro.Testing;
+using MySpot.Services.Availability.Core.Entities;
+using MySpot.Services.Availability.Core.Repositories;
+using MySpot.Services.Availability.Core.ValueObjects;
+using MySpot.Services.Availability.Infrastructure.DAL.Repositories;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace MySpot.Services.Availability.Tests.Contract.Provider;
 
 [ExcludeFromCodeCoverage]
-public class ParkingSpotsConsumerContractsTests : IDisposable
+public class ReservationsConsumerContractsTests : IDisposable
 {
     [Fact]
-    public async Task should_honour_add_resource_pact_with_parking_spots_consumer()
+    public async Task should_honour_get_resource_pact_with_reservations_consumer()
     {
         var pactFile = _endpointContract.GetPactFile();
+        var resourceId = Guid.Parse("00000000-0000-0000-0000-000000000001");
         await _testServer.StartAsync();
+        await _resourcesRepository.AddAsync(Resource.Create(resourceId, 2, new[] { new Tag("test") }));
         
         _endpointContract.Verifier
             .ServiceProvider(_endpointContract.Provider, _testServer.Url)
@@ -24,15 +30,17 @@ public class ParkingSpotsConsumerContractsTests : IDisposable
     }
 
     #region Arrange
-
+    
     private readonly TestDatabase _testDatabase;
+    private readonly IResourcesRepository _resourcesRepository;
     private readonly EndpointContract _endpointContract;
     private readonly TestServer _testServer;
 
-    public ParkingSpotsConsumerContractsTests(ITestOutputHelper output)
+    public ReservationsConsumerContractsTests(ITestOutputHelper output)
     {
         _testDatabase = new TestDatabase();
-        _endpointContract = new EndpointContract("ParkingSpots", "Availability", output);
+        _resourcesRepository = new ResourcesRepository(_testDatabase.Context);
+        _endpointContract = new EndpointContract("Reservations", "Availability", output);
         _testServer = new TestServer("MySpot.Services.Availability.Api", output);
     }
 
